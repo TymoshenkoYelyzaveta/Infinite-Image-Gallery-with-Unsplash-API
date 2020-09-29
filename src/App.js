ecf3c6ee016db777bea1bffd1818972d74214666
Liza <tymoshenkoyelyzaveta.s@gmail.com>
2020-09-29 13:52:12 +0200

    connect to unsplash api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+const accessKey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
+
 const App = () => {
+  const [images, setImages] = useState([]);
+  useEffect(() => {
+    console.log(accessKey);
+    fetch(`https://api.unsplash.com/photos/?client_id=${accessKey}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data);
+      });
+  });
+
+  // if (!accessKey) {
+  //   return (
+  //     <a href='https://api.unsplash.com/developers'>
+  //       Get Your Unsplash API Key
+  //     </a>
+  //   );
+  // }
+
   return (
     <div className='app'>
       <h1>Unsplash Image Gallery</h1>
@@ -11,9 +31,9 @@ const App = () => {
       </form>
 
       <div className='grid'>
-        {[...Array(100)].map((_, index) => (
+        {images.map((image, index) => (
           <div className='image' key={index}>
-            <img src='https://placekitten.com/g/1920/1080' alt='Sample' />
+            <img src={image.urls.regular} alt={image.alt_description} />
           </div>
         ))}
       </div>
