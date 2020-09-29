c957131235606dbb3536001c4f8263d97a15425c
Liza <tymoshenkoyelyzaveta.s@gmail.com>
2020-09-29 10:13:59 +0200

    initia; commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>Unsplash Image Gallery</h1>
+      <form>
+        <input type='text' placeholder='Search ...' />
+        <button>Search</button>
+      </form>
+
+      <div className='grid'>
+        {[...Array(100)].map((_, index) => (
+          <div className='image' key={index}>
+            <img src='https://placekitten.com/g/1920/1080' alt='Sample' />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
